c3a1ebb5e13192cf2123a5d6f8dbf29f2bf88cf1
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 20:03:55 +0300

    Add model class for user data

---
+++ src/main/java/com/anna/news_portal/models/User.java
@@ -0,0 +1,55 @@
+package com.anna.news_portal.models;
+
+public class User {
+  private int id;
+  private String name;
+  private String position;
+  private String role;
+  private int department_id;
+
+  public User(String name, String position, String role) {
+    this.name = name;
+    this.position = position;
+    this.role = role;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public void setPosition(String position) {
+    this.position = position;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public int getDepartment_id() {
+    return department_id;
+  }
+
+  public void setDepartment_id(int department_id) {
+    this.department_id = department_id;
+  }
+}
