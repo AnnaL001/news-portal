0ad86f3eb0654d6c4529be3634f37d277b8f18c7
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 20:02:53 +0300

    Add SQL file to create database structure

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE news_portal;
+\c news_portal;
+
+CREATE TABLE departments (
+  id SERIAL PRIMARY KEY,
+  name varchar,
+  description varchar,
+  employee_count int
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name varchar,
+  position varchar,
+  role varchar,
+  department_id int
+);
+
+CREATE TABLE news (
+  id SERIAL PRIMARY KEY,
+  title varchar,
+  content varchar,
+  user_id int,
+  department_id int,
+  created_at timestamp
+);
+
+CREATE TABLE topics (
+  id SERIAL PRIMARY KEY,
+  name varchar
+);
+
+CREATE TABLE news_topics (
+  id SERIAL PRIMARY KEY,
+  news_id int,
+  topic_id int
+);
+
+CREATE DATABASE news_portal_test WITH TEMPLATE news_portal;
