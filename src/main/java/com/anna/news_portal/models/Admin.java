a9220826f863dbec49f0376d00788bca64f4270d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 20:28:58 +0300

    Add model class representing an Admin's data

---
+++ src/main/java/com/anna/news_portal/models/Admin.java
@@ -0,0 +1,10 @@
+package com.anna.news_portal.models;
+
+public class Admin extends User{
+  public static final String ROLE = "Admin";
+
+  public Admin(String name, String position) {
+    super(name, position);
+    this.role = ROLE;
+  }
+}
