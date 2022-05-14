4be3a01af42931792d141d650a22f609908cae1d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 20:19:09 +0300

    Add model class representing news relating to a department

---
+++ src/main/java/com/anna/news_portal/models/DepartmentNews.java
@@ -0,0 +1,24 @@
+package com.anna.news_portal.models;
+
+import com.anna.news_portal.base.News;
+
+public class DepartmentNews extends News {
+  private int department_id;
+  private static final String NEWS_TYPE = "Departmental";
+
+  public DepartmentNews(String title, String content) {
+    this.title = title;
+    this.content = content;
+    setFormatted_created_date();
+    this.formatted_created_date = getFormatted_created_date();
+    this.news_type = NEWS_TYPE;
+  }
+
+  public int getDepartment_id() {
+    return department_id;
+  }
+
+  public void setDepartment_id(int department_id) {
+    this.department_id = department_id;
+  }
+}
