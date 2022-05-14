95115e9cc982cc33f8334774dc388b91e37c7c41
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 20:18:54 +0300

    Add model class representing news relating to the whole company

---
+++ src/main/java/com/anna/news_portal/models/GeneralNews.java
@@ -0,0 +1,14 @@
+package com.anna.news_portal.models;
+
+import com.anna.news_portal.base.News;
+
+
+public class GeneralNews extends News {
+  private static final String NEWS_TYPE = "General";
+
+  public GeneralNews(String title, String content) {
+    this.title = title;
+    this.content = content;
+    this.news_type = NEWS_TYPE;
+  }
+}
