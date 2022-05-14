f5976070dae615ec9a6ae0373b6d2cd0e84b09a7
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-15 00:00:52 +0300

    Add tests to check for correct instantiation of DepartmentNews class instance

---
+++ src/test/java/com/anna/news_portal/models/DepartmentNewsTest.java
@@ -0,0 +1,35 @@
+package com.anna.news_portal.models;
+
+import com.anna.news_portal.parameter_resolvers.DepartmentNewsParameterResolver;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(DepartmentNewsParameterResolver.class)
+class DepartmentNewsTest {
+  @Test
+  @DisplayName("Test that DepartmentNews class instance instantiates correctly")
+  public void newDepartmentNews_instantiatesCorrectly_true(DepartmentNews departmentNews) {
+    assertNotNull(departmentNews);
+  }
+
+  @Test
+  @DisplayName("Test that DepartmentNews class instance instantiates with title")
+  public void newDepartmentNews_instantiatesWithTitle_true(DepartmentNews departmentNews) {
+    assertEquals("Email 2FA authentication", departmentNews.getTitle());
+  }
+
+  @Test
+  @DisplayName("Test that DepartmentNews class instance instantiates with content")
+  public void newDepartmentNews_instantiatesWithContent_true(DepartmentNews departmentNews) {
+    assertEquals("The 2FA authentication adds an extra layer of security thus ensure security of accounts", departmentNews.getContent());
+  }
+
+  @Test
+  @DisplayName("Test that DepartmentNews class instance instantiates with news type")
+  public void newDepartmentNews_instantiatesWithNewsType_true(DepartmentNews departmentNews) {
+    assertEquals(DepartmentNews.NEWS_TYPE, departmentNews.getNews_type());
+  }
+}
