13a9789af6f411f285273c7f499ae991756ead09
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 23:51:04 +0300

    Add parameter resolver for test dependency injection

---
+++ src/test/java/com/anna/news_portal/parameter_resolvers/GeneralNewsParameterResolver.java
@@ -0,0 +1,19 @@
+package com.anna.news_portal.parameter_resolvers;
+
+import com.anna.news_portal.models.GeneralNews;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class GeneralNewsParameterResolver implements ParameterResolver {
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return parameterContext.getParameter().getType() == GeneralNews.class;
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return new GeneralNews("Change in organizational email policy", "Emails are required to have 2FA authentication set");
+  }
+}
