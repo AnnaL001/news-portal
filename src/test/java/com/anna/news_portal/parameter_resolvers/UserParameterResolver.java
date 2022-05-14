32977e157fa9a15699e8fe10ce785ffb31381ed4
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 22:47:02 +0300

    Add user parameter resolver for test dependency injection

---
+++ src/test/java/com/anna/news_portal/parameter_resolvers/UserParameterResolver.java
@@ -0,0 +1,20 @@
+package com.anna.news_portal.parameter_resolvers;
+
+import com.anna.news_portal.models.User;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class UserParameterResolver implements ParameterResolver {
+
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return parameterContext.getParameter().getType() == User.class;
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return new User("Jane Doe", "Chief Financial Officer");
+  }
+}
