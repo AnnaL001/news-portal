5f837f85a01a88a0988b88b04b11e9823378f88c
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-14 22:35:27 +0300

    Add department news parameter resolver for test dependency injection

---
+++ src/test/java/com/anna/news_portal/parameter_resolvers/DepartmentNewsParameterResolver.java
@@ -0,0 +1,19 @@
+package com.anna.news_portal.parameter_resolvers;
+
+import com.anna.news_portal.models.DepartmentNews;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class DepartmentNewsParameterResolver implements ParameterResolver {
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return parameterContext.getParameter().getType() == DepartmentNews.class;
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return new DepartmentNews("Email 2FA authentication","The 2FA authentication adds an extra layer of security thus ensure security of accounts");
+  }
+}
