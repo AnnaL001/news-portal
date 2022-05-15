a739f04d7fd72984a0bb87daa5d7fe5346460032
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-15 12:04:04 +0300

    Add tests for topics table insert, select and delete operations

--- src/test/java/com/anna/news_portal/dao/Sql2oTopicDaoTest.java
+++ src/test/java/com/anna/news_portal/dao/Sql2oTopicDaoTest.java
@@ -1,11 +1,17 @@
 package com.anna.news_portal.dao;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import com.anna.news_portal.models.Topic;
+import com.anna.news_portal.parameter_resolvers.TopicParameterResolver;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(TopicParameterResolver.class)
 class Sql2oTopicDaoTest {
   private static Connection connection;
   private static Sql2oTopicDao topicDao;
@@ -17,7 +23,50 @@ class Sql2oTopicDaoTest {
     connection = sql2o.open();
   }
 
+  @Test
+  @DisplayName("Test that a news topic can be added")
+  public void add_addsNewsTopic_true(Topic topic) {
+    topicDao.add(topic);
+    assertTrue(topicDao.getAll().contains(topic));
+  }
 
+  @Test
+  @DisplayName("Test that a news topic's id is set to a database generated id")
+  public void add_setsNewsTopicId_true(Topic topic) {
+    int initialId = topic.getId();
+    topicDao.add(topic);
+    assertNotEquals(initialId, topic.getId());
+  }
+
+  @Test
+  @DisplayName("Test that a list of added topics can be retrieved")
+  public void getAll_returnsTopicsList_true(Topic topic) {
+    topicDao.add(topic);
+    Topic[] topics = {topic};
+    assertEquals(Arrays.asList(topics), topicDao.getAll());
+  }
+
+  @Test
+  @DisplayName("Test that an empty list is returned if no topics are listed in the database")
+  public void getAll_returnsEmptyListIfNoTopics_true() {
+    assertEquals(0, topicDao.getAll().size());
+  }
+
+  @Test
+  @DisplayName("Test that a topic can be retrieved")
+  public void get_returnsTopic_true(Topic topic) {
+    topicDao.add(topic);
+    Topic foundTopic = topicDao.get(topic.getId());
+    assertEquals(topic, foundTopic);
+  }
+
+  @Test
+  @DisplayName("Test that all topics listed can be deleted")
+  public void deleteAll_deletesAllTopics_true(Topic topic) {
+    topicDao.add(topic);
+    topicDao.deleteAll();
+    assertEquals(0, topicDao.getAll().size());
+  }
 
   @AfterEach
   public void tearDown() {
