aa27fdb7773f5929d36a058aff67c0936473c50e
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-15 09:49:32 +0300

    Add test for departments table insert operation

--- src/test/java/com/anna/news_portal/dao/Sql2oDepartmentDaoTest.java
+++ src/test/java/com/anna/news_portal/dao/Sql2oDepartmentDaoTest.java
@@ -1,13 +1,15 @@
 package com.anna.news_portal.dao;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import com.anna.news_portal.models.Department;
+import com.anna.news_portal.parameter_resolvers.DepartmentParameterResolver;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(DepartmentParameterResolver.class)
 class Sql2oDepartmentDaoTest {
   private static Connection connection;
   private static Sql2oDepartmentDao departmentDao;
@@ -17,6 +19,13 @@ class Sql2oDepartmentDaoTest {
     Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news_portal_test", "anna", "pol1234");
     departmentDao = new Sql2oDepartmentDao(sql2o);
     connection = sql2o.open();
+  }
+
+  @Test
+  @DisplayName("Test that a department's data can be added to the database")
+  public void add_addsDepartment_true(Department department) {
+    departmentDao.add(department);
+    assertTrue(departmentDao.getAll().contains(department));
   }
 
   @AfterEach
