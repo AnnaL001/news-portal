4d30bb89ac5b7bcf05c24f6cd031b3fd7721a32f
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-15 16:00:10 +0300

    Update model class representing department news data

--- src/main/java/com/anna/news_portal/models/DepartmentNews.java
+++ src/main/java/com/anna/news_portal/models/DepartmentNews.java
@@ -6,9 +6,11 @@ public class DepartmentNews extends News {
   private int department_id;
   public static final String NEWS_TYPE = "Departmental";
 
-  public DepartmentNews(String title, String content) {
+  public DepartmentNews(String title, String content, int user_id, int department_id) {
     this.title = title;
     this.content = content;
+    this.user_id = user_id;
+    this.department_id = department_id;
     setFormatted_created_date();
     this.formatted_created_date = getFormatted_created_date();
     this.news_type = NEWS_TYPE;
