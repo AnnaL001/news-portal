4943cd90239a7d014c57f9f03db00e03d36b1cc5
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-15 06:14:49 +0300

    Add tests to check for correct working of setter methods

--- src/test/java/com/anna/news_portal/models/UserTest.java
+++ src/test/java/com/anna/news_portal/models/UserTest.java
@@ -1,5 +1,6 @@
 package com.anna.news_portal.models;
 
+import com.anna.news_portal.parameter_resolvers.DepartmentParameterResolver;
 import com.anna.news_portal.parameter_resolvers.UserParameterResolver;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(UserParameterResolver.class)
+@ExtendWith(DepartmentParameterResolver.class)
 class UserTest {
   @Test
   @DisplayName("Test that a User class instance instantiates correctly")
@@ -43,5 +45,41 @@ class UserTest {
   @DisplayName("Test that a User class instance instantiates with a default department ID")
   public void newUser_instantiatesWithDefaultDepartmentID(User user) {
     assertEquals(0, user.getDepartment_id());
+  }
+
+  @Test
+  @DisplayName("Test that a user's id is set as specified")
+  public void setId_setsIDCorrectly_true(User user) {
+    user.setId(1);
+    assertEquals(1, user.getId());
+  }
+
+  @Test
+  @DisplayName("Test that a user's name is set as specified")
+  public void setName_setsNameCorrectly_true(User user) {
+    user.setName("Logan Doe");
+    assertEquals("Logan Doe", user.getName());
+  }
+
+  @Test
+  @DisplayName("Test that a user's position is set as specified")
+  public void setPosition_setsPositionCorrectly_true(User user) {
+    user.setPosition("Marketing Director");
+    assertEquals("Marketing Director", user.getPosition());
+  }
+
+  @Test
+  @DisplayName("Test that a user's role is set as specified")
+  void setRole_setsRoleCorrectly_true(User user) {
+    user.setRole("Normal users");
+    assertEquals("Normal users", user.getRole());
+  }
+
+  @Test
+  @DisplayName("Test that a user's department is set as specified")
+  void setDepartment_id_setsDepartmentCorrectly_true(User user, Department department) {
+    department.setId(1);
+    user.setDepartment_id(department.getId());
+    assertEquals(1, user.getDepartment_id());
   }
 }
