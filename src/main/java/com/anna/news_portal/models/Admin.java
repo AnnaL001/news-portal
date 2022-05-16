15b3458fd81d4d8ad17b66dc9eb09f63cb0a2526
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-16 12:26:55 +0300

    Update model class representing Admin data

--- src/main/java/com/anna/news_portal/models/Admin.java
+++ src/main/java/com/anna/news_portal/models/Admin.java
@@ -3,8 +3,8 @@ package com.anna.news_portal.models;
 public class Admin extends User{
   public static final String ROLE = "Admin";
 
-  public Admin(String name, String position, int department_id) {
-    super(name, position, department_id);
+  public Admin(String name, String position) {
+    super(name, position);
     this.role = ROLE;
   }
 
