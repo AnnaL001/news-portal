9e09c89ee58bf17d9a352e2e280eb61c14e41a98
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-16 12:20:57 +0300

    Update model class representing general news posts

--- src/main/java/com/anna/news_portal/models/GeneralNews.java
+++ src/main/java/com/anna/news_portal/models/GeneralNews.java
@@ -6,10 +6,10 @@ import com.anna.news_portal.base.News;
 public class GeneralNews extends News {
   public static final String NEWS_TYPE = "General";
 
-  public GeneralNews(String title, String content, int user_id) {
+  public GeneralNews(String title, String content) {
     this.title = title;
     this.content = content;
-    this.user_id = user_id;
+    this.user_id = 0;
     setFormatted_created_date();
     this.formatted_created_date = getFormatted_created_date();
     this.news_type = NEWS_TYPE;
